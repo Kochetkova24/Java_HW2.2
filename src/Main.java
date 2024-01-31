c0666f6a69299918371cdd824a270f52890f13d8
Kochetkova Tanya <kta240190@gmail.com>
2024-01-31 23:09:22 +0500

    added project

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int initialBalance = 300;
+        int payment = 1000;
+        int bonus;
+        if (payment >= 1000) {
+         bonus = payment/100;
+                } else {
+             bonus = 0;
+        }
+       int balance = initialBalance + payment + bonus;
+        System.out.println("Ваш баланс " + balance + " рублей");
+
+        }
+    }
